7099ffacd9b947d0bbfbc86661d84e7f396357c2
Marcilio Júnior <jrsmarcilio@gmail.com>
2023-12-22 01:57:27 -0300

    update user model

--- src/models/users/interfaces/user.interface.ts
+++ src/models/users/interfaces/user.interface.ts
@@ -1,11 +1,25 @@
+import { AddressEntity } from "src/models/addresses/entities/address.entity";
+import { RoleEntity } from "src/models/roles/entities/roles.entity";
+import { UserSettingsEntity } from "src/models/user_settings/entities/userSettings.entity";
+
 export interface User {
-  id: number;
-  first_name: string;
-  last_name: string;
+  id?: string;
+  name: string;
   email: string;
-  mobile_number: string;
-  phone_number: string;
-  notes: string;
-  timezone: string;
-  language: string;
+  picture: string;
+  birthday: string;
+  phone: string;
+
+  // Status
+  blocked: boolean;
+  deleted: boolean;
+
+  // Timestamps
+  createdAt?: Date;
+  updatedAt?: Date;
+
+  // Relations
+  address?: AddressEntity;
+  role?: RoleEntity;
+  settings?: UserSettingsEntity;
 }
